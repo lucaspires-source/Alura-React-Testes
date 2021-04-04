bb02edfaa7d10debaefd13fd1d23bb7dd7c65fd1
lucaspires-source <pires.lucas94@gmail.com>
2021-04-04 06:52:40 -0300

    Aula 02.02 Como testar uma função

--- src/App.test.js
+++ src/App.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import App from './App.js';
+import App,{calcularNovoSaldo} from './App.js';
 import {render,screen} from '@testing-library/react';
 describe('Componente Principal', () =>{
   describe('Quando eu abro o app do banco',()=>{
@@ -17,6 +17,26 @@ describe('Componente Principal', () =>{
       expect(screen.getByText('Realizar operação')).toBeInTheDocument();
     })
   })
+  describe('Quando realizo uma transação', () =>{
+    it('um saque,o valor irá diminuir',()=>{
+      const valores ={
+        transacao:'saque',
+        valor:50
+      }
+      const novoSaldo = calcularNovoSaldo(valores,150)
+      expect(novoSaldo).toBe(100)
+    })
   
+  })
+  it('um depósito,o valor irá aumentar',()=>{
+    const valores ={
+      transacao:'deposito',
+      valor:50
+    }
+    const novoSaldo = calcularNovoSaldo(valores,150)
+    expect(novoSaldo).toBe(200)
+  })
+
 })
+
 
