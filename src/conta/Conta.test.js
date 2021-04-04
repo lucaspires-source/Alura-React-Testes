36605d91c0e584ada83474d629565e90d7bb8518
lucaspires-source <pires.lucas94@gmail.com>
2021-04-04 10:27:57 -0300

    Aula 04.07 Simulando uma função

--- src/conta/Conta.test.js
+++ src/conta/Conta.test.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Conta from './Conta'
 import {
+  fireEvent,
   render,
   screen,
 } from "@testing-library/react";
@@ -12,5 +13,13 @@ describe('Componente de conta', ()=>{
     
 
         expect(saldo.textContent).toBe('R$ 1000')
+    })
+
+    it('Chama a função de realizar transação quando o botão é clicado',()=>{
+
+      const funcaoRealizarTransacao = jest.fn()
+      render(<Conta saldo={1000}  realizarTransacao={funcaoRealizarTransacao}/>)
+      fireEvent.click(screen.getByText('Realizar operação'))
+      expect(funcaoRealizarTransacao).toHaveBeenCalled()
     })
 })
