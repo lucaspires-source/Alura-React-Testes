8dec3d34520c7713ef9d869d0e7bedb1731a3e5a
lucaspires-source <pires.lucas94@gmail.com>
2021-04-04 10:10:07 -0300

    Aula 04.05 Testando Código assíncrono

--- src/AppIntegracoes.test.js
+++ src/AppIntegracoes.test.js
@@ -8,23 +8,24 @@ import api from "./api";
 
 jest.mock('./api')
 describe("Requisições para API", () => {
-    it('Exibir lista de transações através da Api', () =>{
+    it('Exibir lista de transações através da Api', async () =>{
         api.listaTransacoes.mockResolvedValue([
             {
-                "valor": 10,
+                "valor": "10",
                 "transacao": "saque",
                 "data": "10/08/2020",
                 "id": 1
               },
               {
                 "transacao": "deposito",
-                "valor": 20,
+                "valor": "20",
                 "data": "26/09/2020",
                 "id": 2
               }
         ])
 
         render(<App/>)
+        expect(await screen.findByText('saque')).toBeInTheDocument();
         expect(screen.getByTestId('transacoes').children.length).toBe(2)
     })
 })
