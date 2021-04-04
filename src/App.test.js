30779cc94e915caba4420bd09265b932cbce47ce
lucaspires-source <pires.lucas94@gmail.com>
2021-04-04 06:37:58 -0300

    Aula 01.04 Criando o Primeiro Teste

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,9 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+import React from 'react';
+import App from './App.js';
+import {render,screen} from '@testing-library/react';
+describe('Componente Principal', () =>{
+  it('Mostrar o Nome do Banco',() =>{
+    render(<App/>)
+    expect(screen.getByText('ByteBank')).toBeInTheDocument();
+  })
+})
